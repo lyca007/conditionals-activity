f8068ba37fce48fdacc5aecaf068b3057c30c69e
lyca007 <202580212@psu.palawan.edu.ph>
2026-07-29 20:22:06 +0800

    Create problem2.cs

---
+++ problem2.cs
@@ -0,0 +1,12 @@
+class Program
+{
+  static void Main(string[] args)
+  {
+    string firstName = "Maria";
+    string lastName = "Santos";
+
+    string fullName = firstName + " " + lastName;
+
+    Console.WriteLine("Full Name: " + fullName);
+  }
+}

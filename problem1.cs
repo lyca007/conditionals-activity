1fd709183f682aca38cae195052d36c2868482a6
lyca007 <202580212@psu.palawan.edu.ph>
2026-07-29 20:19:09 +0800

    Create problem1.cs

---
+++ problem1.cs
@@ -0,0 +1,10 @@
+class Program
+{
+  static void Main(string[] args)
+  {
+    double radius = 5;
+
+    double area = Math.PI * radius;
+    Console.WriteLine("The area is : " + area.ToString("F2"));
+  }
+}

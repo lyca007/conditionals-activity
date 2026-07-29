6501743a33727a02ee2f9f0571b30b294419d66b
lyca007 <202580212@psu.palawan.edu.ph>
2026-07-29 21:13:40 +0800

    Update problem1.cs

--- problem1.cs
+++ problem1.cs
@@ -2,9 +2,32 @@ class Program
 {
   static void Main(string[] args)
   {
-    double radius = 5;
+    //Integer
+    Console.Write("Enter first whole number: ");
+    Console.Write("Enter second whole number: ");
+    int num1 = int.Parse(Console.ReadLine());
+    int num2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Sum: " + (num1 + num2));
 
-    double area = Math.PI * radius;
-    Console.WriteLine("The area is : " + area.ToString("F2"));
+    //Float
+    Console.Write("\nFirst score: ");
+    Console.Write("Second score: ");
+    Console.Write("Third score: ");
+    float s1 = float.Parse(Console.ReadLine());
+    float s2 = float.Parse(Console.ReadLine());
+    float s3 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Average: " + ((s1+s2+s3)/3).ToString("F2"));
+
+    //Double
+    Console.Write("\nRadius: ");
+    double r = double.Parse(Console.ReadLine());
+    Console.WriteLine("Area: " + (Math.PI * r * r));
+
+    //String
+    Console.Write("\nFirst name: ");
+    Console.Write("Last name: ");
+    string fn = Console.Readline();
+    string ln = Console.Readline();
+    Console.WriteLine("Full name: " + fn + " " + ln);
   }
 }
